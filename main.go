034673a6e02c5ec2cb09dffe3d66b2e2c501831a
David Duarte <davidandradeduarte@gmail.com>
2022-07-22 16:17:17 +0100

    feat: upgrade to benthos4; use new azure_table_storage output version from fork

--- main.go
+++ main.go
@@ -1,9 +1,43 @@
 package main
 
 import (
-	"github.com/Jeffail/benthos/v3/lib/service"
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"time"
+
+	_ "github.com/benthosdev/benthos/v4/public/components/all"
+	"github.com/benthosdev/benthos/v4/public/service"
 )
 
 func main() {
-	service.Run()
+	panicOnErr := func(err error) {
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	content, err := ioutil.ReadFile("pipeline.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	builder := service.NewStreamBuilder()
+	err = builder.SetYAML(string(content))
+	panicOnErr(err)
+
+	stream, err := builder.Build()
+	panicOnErr(err)
+
+	ctx, _ := context.WithCancel(context.Background())
+	go stream.Run(ctx)
+	fmt.Println("waiting")
+	stream.StopWithin(10 * time.Second)
+	fmt.Println("stopping")
+	// cancel()
+	// fmt.Println("canceled")
+	fmt.Println("waiting ...")
+	time.Sleep(15 * time.Second)
+	fmt.Println("done")
 }
